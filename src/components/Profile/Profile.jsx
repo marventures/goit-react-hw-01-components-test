0bff9fcf065dd099a00fe06f22ebef5cea43581b
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2024-01-13 21:09:31 +0800

    Update Profile prop types

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 import css from './Profile.module.css';
 
-const Profile = ({ username, tag, location, avatar, stats }) => {
+export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
     <div className={css.profile}>
       <div className={css.description}>
@@ -29,4 +29,14 @@ const Profile = ({ username, tag, location, avatar, stats }) => {
   );
 };
 
-export default Profile;
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
