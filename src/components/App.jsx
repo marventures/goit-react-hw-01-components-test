764db696088dd1b7f690bed855c05b22c8518724
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2024-01-13 21:09:51 +0800

    Add Statistics component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
-import Profile from './Profile/Profile';
+import { Profile } from './Profile/Profile';
 import user from '../data/user.json';
+import { Statistics } from './Statistics/Statistics';
+import data from '../data/data.json';
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload Stats" stats={data} />
+    </>
   );
 };
